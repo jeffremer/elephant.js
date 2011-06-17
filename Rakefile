18ce8edc0e7abec7b2c5a415e37f742ebaff9f9a
Jeff Remer <jeff@threestarchina.com>
2011-06-17 16:38:29 -0700

    Updated docs and added rocco task to the Rakefile

--- Rakefile
+++ Rakefile
@@ -1,9 +1,20 @@
-
 begin
   require 'jasmine'
   load 'jasmine/tasks/jasmine.rake'
 rescue LoadError
   task :jasmine do
     abort "Jasmine is not available. In order to run jasmine, you must: (sudo) gem install jasmine"
+  end
+end
+
+begin
+  require 'rocco/tasks'
+  Rocco::make 'docs/', 'src/*.js', {
+      :language => 'javascript',
+      :comment_chars => '//'
+  }  
+rescue LoadError
+  task :rocco do
+    abort "Rocco isn't available. In order to build docs, you must: (sudo) gem install rocco"
   end
 end
--- src/elephant.js
+++ src/elephant.js
@@ -1,3 +1,13 @@
+//		   ______              __           __        _    
+//		  / __/ /___ ___ ___  / / ___ ____ / /_      (_)___
+//		 / _// // -_) -_) _ \/ _ | _ `/ _ | __/_    / /(_-<
+//		/___/_/ \__/\__/ .__/_//_|_,_/_//_|__/(_)__/ //___/
+//   	              /_/                       |___/                                         
+//	
+// elephant.js 0.0.1 (c) 2011 Jeff Remer
+//
+// MIT License, see [LICENSE](https://github.com/jeffremer/elephant.js/blob/master/LICENSE)
+
 (function(){
 	var Elephant;
 	typeof exports != 'undefined' ? Elephant = exports : Elephant = this.Elephant = {};
@@ -9,6 +19,8 @@
 	
 	// `each` and `map`
 	// Taken from [underscore.js](http://documentcloud.github.com/underscore)
+	// Rather than make underscore a dependency, here are standalone versions
+	// of Jeremy Ashkenas wonderful each and map functions.
 	function each(obj, iterator, context) {
 		if (obj == null) return;
 		if (Array.prototype.forEach && obj.forEach === Array.prototype.forEach) {
@@ -37,11 +49,13 @@
 	}
 	
 	// Elephant.has
-	// ------------
+	// ------------   
 	Elephant.has = {
+		// Determines if window.localStorage is available
 		localStorage : function() {
 			try { return !!localStorage.getItem; } catch (e) {return false};
 		}(),
+		// Tests to make sure cookies are available
 		cookies: function() {
 			if(navigator.cookieEnabled) return true;
 			document.cookie = 'elephantcookie=1';
@@ -51,14 +65,21 @@
 		}()
 	};
 	
+	// Elephant.Trunk
+	// --------------
+	
+	// The main store class.
 	Elephant.Trunk = function(name) {
+		// `name` is a namespace for the localStorage object
 		this.name = name;
 		var store = localStorage.getItem(this.name);
+		// `Elephant.Trunk.data` is JSON hash of guid to object
 		this.data = (store && JSON.parse(store)) || {};
 	};
-
+    
+	// `guid` generates a random 32-bit unique ID
 	Elephant.Trunk.prototype.guid = function() {
-	    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	    return 'xxxxxxxx-xxxx-xxxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 		    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 		    return v.toString(16);
 		});
@@ -71,9 +92,18 @@
 	// CRUD
 	// ====
 
+	// Create
+	// ------
+
 	// `save` creates a new guid for the object, assigns
 	// the guid, updates the memory store, then syncs the
 	// local store.
+	
+	// `guid` a very low chance of colliding, even so the `save` method
+	// checks to see if the `guid` is in use and generates a new one before
+	// clobbering any existing objects.
+	// TODO If the object has a guid update instead of save.
+	//		store.save({...}) // -> {..., guid: '53b425a6-47cb-462c-9498-ba236becee06'}   
 	Elephant.Trunk.prototype.save = function(object) {
 		var uuid = this.guid();
 		while(uuid in this.data) uuid = guid();
@@ -83,8 +113,27 @@
 		return object;
 	};
 	
+	// Read
+	// ------
+		
+	// `find` retrieves an object by guid.
+	//		store.find('53b425a6-47cb-462c-9498-ba236becee06') // -> {...}
+	Elephant.Trunk.prototype.find = function(guid) {		
+		return this.data[guid]
+	};
+
+	// `findAll` retrieves all of the objects as an array.
+    //		store.findAll() // -> [{...}, {...}, {...}]
+	Elephant.Trunk.prototype.findAll = function() {		
+		return map(this.data, function(obj){return obj;})
+	};
+	
+	// Update
+	// ------	
+	
 	// `update` saves if the object has no guid, or
 	// it replaces the object in memory store and syncs.
+	// 		store.update({..., guid: '53b425a6-47cb-462c-9498-ba236becee06'}) // -> {..., guid: '53b425a6-47cb-462c-9498-ba236becee06'}
 	Elephant.Trunk.prototype.update = function(object) {
 		if(!object.guid) {
 			object = this.save(object);
@@ -95,14 +144,12 @@
 		return object;
 	};
 	
-	Elephant.Trunk.prototype.find = function(guid) {		
-		return this.data[guid]
-	};
-	
-	Elephant.Trunk.prototype.findAll = function() {		
-		return map(this.data, function(obj){return obj;})
-	};
+	// Destroy
+	// ------		
 
+	// `destroy` removes the object from the store and deletes
+	// the guid off the object before returning it.
+	//		store.destroy({..., guid: '53b425a6-47cb-462c-9498-ba236becee06'}) // -> {...}
 	Elephant.Trunk.prototype.destroy = function(object) {
 		if(object.guid && object.guid in this.data) {
 			delete this.data[object.guid];
@@ -112,6 +159,8 @@
 		return object;
 	};
 	
+	// `destroyAll` clears the internal data store then saves the blank object.
+	//		store.destroyAll() // -> undefined	
 	Elephant.Trunk.prototype.destroyAll = function() {
 		this.data = {};
 		this.sync();
